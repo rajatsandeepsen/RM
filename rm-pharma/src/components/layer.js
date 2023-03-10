2ea0129af3b7b002f47ea4c4ae8110b053cd17fa
Rajat Sandeep <rajatsandeepsen1839@gmail.com>
2023-03-11 02:15:06 +0530

    fix: next/font

--- rm-pharma/src/components/layer.js
+++ rm-pharma/src/components/layer.js
@@ -1,10 +1,12 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import styles from "@/styles/Home.module.scss";
+import { Inter,Montserrat } from 'next/font/google'
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 const Layer = ({children}) => {
     return ( 
-        <main>
+        <main className={montserrat.className}>
             <Header />
             {children}
             <Footer />
